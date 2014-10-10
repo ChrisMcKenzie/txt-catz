75e9e4ba7f517658dec48f722dcb8e8770349c13
ChrisMcKenzie <chris@kbsurfer.com>
2014-10-09 19:48:45 -0700

    checkit

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-    "github.com/sfreiberg/gotwilio"
+	"log"
+	"net/http"
+
+	"github.com/sfreiberg/gotwilio"
 )
 
 // +1 442-242-6855
 
 func main() {
-    accountSid := "ACe3f0bf659491650bbff689790fd574e1"
-    authToken := "c57d2b5b368d0dd291c3c5c908ccc785"
-    twilio := gotwilio.NewTwilioClient(accountSid, authToken)
+	accountSid := "ACe3f0bf659491650bbff689790fd574e1"
+	authToken := "c57d2b5b368d0dd291c3c5c908ccc785"
+	twilio := gotwilio.NewTwilioClient(accountSid, authToken)
 
-    from := "+16193761185"
-    to := "+17605968806"
-    message := "Welcome to gotwilio!"
-    twilio.SendSMS(from, to, message, "", "")
+	from := "+16193761185"
+	to := "+17605968806"
+	message := "send me a command... (hint: go get catz)"
+	twilio.SendSMS(from, to, message, "", "")
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%#v", r)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
